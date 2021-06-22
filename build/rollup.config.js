0c3d44bc1cec5cb571953b01b903752f95b24561
guochao <463561248@qq.com>
2021-06-22 23:47:56 +0800

    代码生成

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,104 +1,79 @@
-/**
- * @type {import('rollup').RollupOptions}
- */
+const fse = require("fs-extra");
+const path = require("path");
 const { babel } = require("@rollup/plugin-babel");
 // const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('@rollup/plugin-commonjs');
-const typescript = require('@rollup/plugin-typescript');
-const postcss = require('rollup-plugin-postcss');
-const path = require('path');
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const postcss = require("rollup-plugin-postcss");
 
-const name = "sasa";
-const file = (type) => `dist/${name}.${type}.js`;
-export default [
-//   {
-//   // input: [
-//   //   'src/index.ts',
-//   //   'src/alert/index.ts',
-//   //   'src/button/index.ts'],
-//   // input: {
-//   //   // index: "src/index.ts",
-//   //   "alert/index": "src/alert/index.ts",
-//   //   "button/index": "src/button/index.ts",
-//   // },
-//   // output: [
-//   //   {
-//   //     dir: "es",
-//   //     format: "es",
-//   //     exports: "auto",
-//   //   },
-//   //   {
-//   //     dir: "cjs",
-//   //     format: "cjs",
-//   //     exports: "auto",
-//   //   },
-//   // ],
-//   input: 'src/alert/index.ts',
-//   output: {
-//     file: 'dist/alert/index.js',
-//     format: 'es',
-//     exports: "auto",
-//   },
-//   plugins: [
-//     // resolve(),
-//     postcss({
-//       extract: true,
-//     }),
-//     typescript(),
-//     commonjs({
-//       extensions: [".js", ".jsx", ".ts", ".tsx"],
-//     }),
-//     babel({
-//       babelHelpers: "bundled",
-//       exclude: "node_modules/**",
-//       // 必须设置 extensions，默认不包括 ts 和 tsx. see: https://babeljs.io/docs/en/babel-core#default_extensions
-//       extensions: [".js", ".jsx", ".ts", ".tsx"],
-//     }),
-//   ],
-// },
-{
-  input: 'src/button/index.ts',
-  output: {
-    file: 'dist/button/index.js',
-    format: 'es',
-    exports: "auto",
-  },
-  external: 
-    (id, parentId, isResolved) => {
-      // console.log('id---', id)
-      // console.log('parentId---', parentId)
-      // console.log('isResolved---', isResolved)
-      if(parentId && !isResolved){
-        const idpath = path.resolve(path.dirname(parentId), id)
-        const p = path.relative(path.resolve(__dirname, '..', 'src/button'), idpath)
-        // console.log('pppp', p)
-        if(p.startsWith('..')){
-          // console.log('<><><><.')
-          return true
-        }
+// 排除那些目录
+const excludeDir = [];
+let componentDir = [];
+// 获取实体类
+const getComponentEntry = () => {
+  // 1. 读取 src 的目录  ----》 找到 所有  目录
+  const dirInfo = fse.readdirSync(path.resolve(__dirname, "../src"), {
+    withFileTypes: true,
+  });
+  // 2. 排除不需要的
+  componentDir = dirInfo
+    .filter((dir) => dir.isDirectory())
+    .map((dir) => dir.name)
+    .filter((name) => !excludeDir.includes(name));
+  console.log("componentDir", componentDir);
+};
+const getInput = (name) => {
+  return `src/${name}/index.ts`;
+};
+
+const getOutput = (name, type) => {
+    return {
+        file: `dist/${name}/index.js`,
+        format: type,
+        exports: "auto",
       }
-      
-      // if(id === 'lodash'){ return true}
-      // if(id === '../alert') {return true}
-      // if(id === './style/index.css') {return true}
-      // if(id === '/Users/saber/coding/mygithub/cat-ui/src/alert/alert.ts') {return true}
-      // if(id === '/Users/saber/coding/mygithub/cat-ui/src/alert/style/index.css') {return true}
-      return false
-    },
-  plugins: [
-    // resolve(),
-    postcss({
-      extract: true,
-    }),
-    typescript(),
-    commonjs({
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-    }),
-    babel({
-      babelHelpers: "bundled",
-      exclude: "node_modules/**",
-      // 必须设置 extensions，默认不包括 ts 和 tsx. see: https://babeljs.io/docs/en/babel-core#default_extensions
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-    }),
-  ],
-}];
+}
+
+const getExternal = (name) => {
+  return (id, parentId, isResolved) => {
+    if(parentId && !isResolved){
+      const idpath = path.resolve(path.dirname(parentId), id)
+      const relPath = path.relative(path.resolve(__dirname, '..', `src/${name}`), idpath)
+      if(relPath.startsWith('..')){
+        return true
+      }
+    }
+    return false;
+  }
+}
+
+const universalPlugins = [
+  // resolve(),
+  postcss({
+    extract: true,
+  }),
+  typescript(),
+  commonjs({
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  }),
+  babel({
+    babelHelpers: "bundled",
+    exclude: "node_modules/**",
+    // 必须设置 extensions，默认不包括 ts 和 tsx. see: https://babeljs.io/docs/en/babel-core#default_extensions
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  }),
+];
+
+const getRollupConfig = () => {
+  getComponentEntry();
+  return componentDir.map((name) => ({
+    input: getInput(name),
+    output: getOutput(name, 'es'),
+    external: getExternal(name),
+    plugins: universalPlugins 
+  }));
+};
+
+export default () => {
+  return getRollupConfig();
+};
