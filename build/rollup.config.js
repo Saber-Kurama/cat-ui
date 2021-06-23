8bcb6a45394083e7c47439c37204ed9b8c186eae
guochao <463561248@qq.com>
2021-06-23 20:17:43 +0800

    调整

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -5,9 +5,11 @@ const { babel } = require("@rollup/plugin-babel");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
 const postcss = require("rollup-plugin-postcss");
+const tailwindcss = require("tailwindcss");
+const postcssImport = require("postcss-import") 
 
 // 排除那些目录
-const excludeDir = [];
+const excludeDir = ['alert'];
 let componentDir = [];
 // 获取实体类
 const getComponentEntry = () => {
@@ -36,8 +38,13 @@ const getOutput = (name, type) => {
 
 const getExternal = (name) => {
   return (id, parentId, isResolved) => {
-    if(parentId && !isResolved){
+    console.log('name--', name)
+    console.log('id', id);
+    console.log('parentId', parentId)
+    console.log('isResolved', isResolved)
+    if(parentId ){
       const idpath = path.resolve(path.dirname(parentId), id)
+      console.log('idpathidpath', idpath)
       const relPath = path.relative(path.resolve(__dirname, '..', `src/${name}`), idpath)
       if(relPath.startsWith('..')){
         return true
@@ -47,10 +54,11 @@ const getExternal = (name) => {
   }
 }
 
-const universalPlugins = [
+const universalPlugins = () => [
   // resolve(),
   postcss({
     extract: true,
+    plugins: [tailwindcss({})]
   }),
   typescript(),
   commonjs({
@@ -66,12 +74,65 @@ const universalPlugins = [
 
 const getRollupConfig = () => {
   getComponentEntry();
-  return componentDir.map((name) => ({
+  const rollupConfig =  componentDir.map((name) => ({
     input: getInput(name),
     output: getOutput(name, 'es'),
     external: getExternal(name),
-    plugins: universalPlugins 
+    plugins: universalPlugins() 
   }));
+  console.log(JSON.stringify(rollupConfig))
+  rollupConfig.unshift(
+    {
+      input: getInput('alert'),
+      output: {
+        file: 'dist/alert/index.js',
+        format: 'es',
+        exports: "auto",
+      },
+      external: getExternal('alert'),
+      // external: 
+      //   (id, parentId, isResolved) => {
+      //     // console.log('id---', id)
+      //     // console.log('parentId---', parentId)
+      //     // console.log('isResolved---', isResolved)
+      //     if(parentId && !isResolved){
+      //       const idpath = path.resolve(path.dirname(parentId), id)
+      //       const p = path.relative(path.resolve(__dirname, '..', 'src/alert'), idpath)
+      //       // console.log('pppp', p)
+      //       if(p.startsWith('..')){
+      //         // console.log('<><><><.')
+      //         return true
+      //       }
+      //     }
+          
+      //     // if(id === 'lodash'){ return true}
+      //     // if(id === '../alert') {return true}
+      //     // if(id === './style/index.css') {return true}
+      //     // if(id === '/Users/saber/coding/mygithub/cat-ui/src/alert/alert.ts') {return true}
+      //     // if(id === '/Users/saber/coding/mygithub/cat-ui/src/alert/style/index.css') {return true}
+      //     return false
+      //   },
+      plugins: universalPlugins(),
+      // plugins: [
+      //   // resolve(),
+      //   postcss({
+      //     extract: true,
+      //     plugins: [tailwindcss()]
+      //   }),
+      //   typescript(),
+      //   commonjs({
+      //     extensions: [".js", ".jsx", ".ts", ".tsx"],
+      //   }),
+      //   babel({
+      //     babelHelpers: "bundled",
+      //     exclude: "node_modules/**",
+      //     // 必须设置 extensions，默认不包括 ts 和 tsx. see: https://babeljs.io/docs/en/babel-core#default_extensions
+      //     extensions: [".js", ".jsx", ".ts", ".tsx"],
+      //   }),
+      // ],
+    },
+    )
+  return rollupConfig;
 };
 
 export default () => {
---
+++ build/rollup.config34.js
@@ -0,0 +1,110 @@
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const { babel } = require("@rollup/plugin-babel");
+// const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('@rollup/plugin-typescript');
+const postcss = require('rollup-plugin-postcss');
+const path = require('path');
+const tailwindcss = require("tailwindcss")
+
+const name = "sasa";
+const file = (type) => `dist/${name}.${type}.js`;
+export default [
+
+{
+  input: 'src/alert/index.ts',
+  output: {
+    file: 'dist/alert/index.js',
+    format: 'es',
+    exports: "auto",
+  },
+  external: 
+    (id, parentId, isResolved) => {
+      // console.log('id---', id)
+      // console.log('parentId---', parentId)
+      // console.log('isResolved---', isResolved)
+      if(parentId && !isResolved){
+        const idpath = path.resolve(path.dirname(parentId), id)
+        const p = path.relative(path.resolve(__dirname, '..', 'src/alert'), idpath)
+        // console.log('pppp', p)
+        if(p.startsWith('..')){
+          // console.log('<><><><.')
+          return true
+        }
+      }
+      
+      // if(id === 'lodash'){ return true}
+      // if(id === '../alert') {return true}
+      // if(id === './style/index.css') {return true}
+      // if(id === '/Users/saber/coding/mygithub/cat-ui/src/alert/alert.ts') {return true}
+      // if(id === '/Users/saber/coding/mygithub/cat-ui/src/alert/style/index.css') {return true}
+      return false
+    },
+  plugins: [
+    // resolve(),
+    postcss({
+      extract: true,
+      plugins: [tailwindcss()]
+    }),
+    typescript(),
+    commonjs({
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      // 必须设置 extensions，默认不包括 ts 和 tsx. see: https://babeljs.io/docs/en/babel-core#default_extensions
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }),
+  ],
+},
+{
+  input: 'src/button/index.ts',
+  output: {
+    file: 'dist/button/index.js',
+    format: 'es',
+    exports: "auto",
+  },
+  external: 
+    (id, parentId, isResolved) => {
+      // console.log('id---', id)
+      // console.log('parentId---', parentId)
+      // console.log('isResolved---', isResolved)
+      if(parentId && !isResolved){
+        const idpath = path.resolve(path.dirname(parentId), id)
+        const p = path.relative(path.resolve(__dirname, '..', 'src/button'), idpath)
+        // console.log('pppp', p)
+        if(p.startsWith('..')){
+          // console.log('<><><><.')
+          return true
+        }
+      }
+      
+      // if(id === 'lodash'){ return true}
+      // if(id === '../alert') {return true}
+      // if(id === './style/index.css') {return true}
+      // if(id === '/Users/saber/coding/mygithub/cat-ui/src/alert/alert.ts') {return true}
+      // if(id === '/Users/saber/coding/mygithub/cat-ui/src/alert/style/index.css') {return true}
+      return false
+    },
+  plugins: [
+    // resolve(),
+    postcss({
+      extract: true,
+      plugins: [tailwindcss()]
+    }),
+    typescript(),
+    commonjs({
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      // 必须设置 extensions，默认不包括 ts 和 tsx. see: https://babeljs.io/docs/en/babel-core#default_extensions
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }),
+  ],
+},
+];
